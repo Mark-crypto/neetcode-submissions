bad057e2487e1dab4be0391e5a940b41b3283568
Mark-crypto <57247444+Mark-crypto@users.noreply.github.com>
2026-08-01 12:05:49 +0300

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int rob(int[] nums) {
+
+        int rob1 = 0;
+        int rob2 = 0;
+
+        for (int num : nums) {
+            int current = Math.max(rob1 + num, rob2);
+            rob1 = rob2;
+            rob2 = current;
+        }
+
+        return rob2;
+    }
+}
